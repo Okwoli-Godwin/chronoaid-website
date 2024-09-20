8a8987496356deeddc2f543e1169d52c6d011cb4
jummie-jk <jummiejk21@gmail.com>
2024-09-20 01:20:21 +0100

    feat: started footer

--- client/src/components/Static/Footer.tsx
+++ client/src/components/Static/Footer.tsx
@@ -1,8 +1,18 @@
-
+import logo from "../../assets/logo-white.png"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className="h-[250px] bg-[#032637] p-12 mt-[50px]">
+       <div className="flex flex-col justify-center items-center">
+         <img src={logo} alt='chronoAid Ai' />
+         <div className='flex flex-row space-x-3 mt-8'>
+          <p className="text-[16px] font-semibold leading-[24px] text-[#EAECF0]" >About</p>
+          <p className="text-[16px] font-semibold leading-[24px] text-[#EAECF0]">Contact Us</p>
+          <p className="text-[16px] font-semibold leading-[24px] text-[#EAECF0]">Who we are</p>
+         </div>
+       </div>
+
+    </section>
   )
 }
 
